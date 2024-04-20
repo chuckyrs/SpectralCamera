1d60e930d2f64fdfc84967d0e6e14e6ab417158c
chuckyrs <chuckyrs@gmail.com>
2024-04-20 16:15:11 +1000

    Tidying comments in AdditionsForAF.cpp

--- additions/src/AdditionsForAF.cpp
+++ additions/src/AdditionsForAF.cpp
@@ -35,7 +35,7 @@ gint AF_Additions::setup(GError** error) {
 
 void AF_Additions::focusAchieved() {
     focussed_value_= focus_value_;
-    g_print("Focussed_Value_ is %f\n", focussed_value_);
+    // g_print("Focussed_Value_ is %f\n", focussed_value_);
     focussed_ = TRUE;
 }
 
@@ -77,11 +77,6 @@ gboolean AF_Additions::releaseFocusLock(gpointer user_data)
   * @param pad    : element pad
   * @param udata  : the gpointer to user data
   */
-/*gboolean AF_Additions::focusImageCapturedWrapper(GstElement* fsink,
-    GstBuffer* buffer, GstPad* pad, gpointer user_data){
-        return reinterpret_cast<AF_Additions*>(user_data)->focusImageCaptured(fsink,
-        buffer, pad, user_data);
-}*/
 
 gboolean AF_Additions::focusImageCaptured(GstElement* fsink,
     GstBuffer* buffer, GstPad* pad, gpointer user_data)
@@ -90,15 +85,10 @@ gboolean AF_Additions::focusImageCaptured(GstElement* fsink,
     GstMapInfo info;
     gfloat difference;
 
-    //g_print("FOCUS IMAGE CAPTURE CALLED\n");
-
     self->additions_parent_->closeFocusValve();
 
-    //if (self->grab_focus_frame_) { //and perhaps && app->focusValue == 0 or NULL
     if (gst_buffer_map(buffer, &info, GST_MAP_READ)) {
-        if (info.size) {
-            //self->grab_focus_frame_ = FALSE;
-            
+        if (info.size) {            
             self->focus_value_= laplacianMean(&info);        }
 
         gst_buffer_unmap(buffer, &info);
@@ -109,16 +99,8 @@ gboolean AF_Additions::focusImageCaptured(GstElement* fsink,
             if (difference < 0)
                 difference = (self->focus_value_ - self->focussed_value_);
 
-            g_print("Focus Value: %f\n", self->focus_value_);
-            g_print("Last Focus Value: %f\n", self->focussed_value_);
-            g_print("Difference check: %f\n", difference);
-
             //Greater than a 10% difference
             if (difference >= (0.1 * self->focussed_value_)) {
-                
-                //g_print("Current Focus Value: %d\n", app->focusValue);
-                //g_print("Best Focus Value: %d\n", app->bestFocusValue);
-                //g_print("Focus Index: %d\n", app->lastSetFocusIndex);
                 self->focussed_ = FALSE;
                 self->focussing_ = TRUE;
             }
@@ -135,11 +117,6 @@ gboolean AF_Additions::focusImageCaptured(GstElement* fsink,
         }
 
     }
-       /* else {
-            //Error Handling in HERE!
-            //NVGST_WARNING_MESSAGE("focus buffer probe failed\n");
-        } 
-    }*/
     return TRUE;
 }
 
@@ -168,17 +145,9 @@ gfloat AF_Additions::laplacianMean (GstMapInfo *padinfo)
     y = resolution_height/2;
 
     cv::Mat frame(cv::Size(resolution_width, resolution_height), CV_8UC1, (char *)padinfo->data, cv::Mat::AUTO_STEP);
-    //cv::Mat frame = cv::Mat((height * 3/2),width, CV_8UC1, (guchar *)padinfo->data, cv::Mat::AUTO_STEP);
     cv::Mat cropped = frame(cv::Range(y-100,y+100), cv::Range(x-100, x+100));
-    //cv::Mat bgrFrame;
-    //cv::Mat grayFrame;
     cv::Mat laplacianFrame;
-
-    //cv::cvtColor(cropped,bgrFrame, cv::COLOR_RGBA2BGR);
-    //cv::cvtColor(bgrFrame, grayFrame, cv::COLOR_BGR2GRAY);
     cv::Laplacian(cropped, laplacianFrame, CV_16U);
-
-    //cv::imshow("Gray Frame", bgrFrame);
 
     return (gfloat)cv::mean(laplacianFrame)[0];
 }
@@ -186,7 +155,6 @@ gfloat AF_Additions::laplacianMean (GstMapInfo *padinfo)
 void AF_Additions::triggerFocusCapture(void)
 {
     additions_parent_->openFocusValve();
-    //grab_focus_frame_ = TRUE;
     focus_value_ = 0;
     focussing_ = TRUE;
 }
@@ -197,9 +165,7 @@ gboolean AF_Additions::focusTriggerWrapper(gpointer user_data) {
 gboolean AF_Additions::focusTrigger(gpointer user_data)
 {
     AF_Additions* self = static_cast<AF_Additions*>(user_data);
-    //g_print("Hitting idleFocus_trigger\n");
     if ((!self->focus_lock_) && (!self->focussing_) && (self->focus_value_ == 0)) {
-        //g_print("Hitting the trigger\n");
         self->triggerFocusCapture();
         return FALSE;
     }
@@ -215,7 +181,6 @@ gboolean AF_Additions::runFocus(gpointer user_data)
     AF_Additions* self = static_cast<AF_Additions*>(user_data);
 
     //Need to set this to run the focus algorithm
-    //g_print ("Running focus with value: %d\n", self->AF_Additions::focus_value_);
     self->focus_machine_.runFocus(self->AF_Additions::focus_value_);
     self->focus_value_ = 0;
     self->focussing_ = FALSE;
